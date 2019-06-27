8bf5d448f17f25e08198e476d2d3cb080385f554
hobeygo <onur.beygo@ing.com>
2019-06-27 23:33:47 +0200

    parametrized_query

--- consumer.py
+++ consumer.py
@@ -2,7 +2,15 @@ import io
 import avro.schema
 import avro.io
 import json
+import mysql.connector as mariadb
 from kafka import KafkaConsumer
+
+#open mariadb cursor
+
+mariadb_connection = mariadb.connect(user='root', password='example', database='luca', port=3306)
+cursor = mariadb_connection.cursor()
+
+
 
 # To consume messages
 CONSUMER = KafkaConsumer('CASA_events',
@@ -22,3 +30,4 @@ for msg in CONSUMER:
     #Getting a particular field out of the row event data
     print(acc_event["EVENT_CODE"])
     print(acc_event["BANK_ACCOUNT_TYPE"])
+    cursor.execute("select Account_code, D/C from rule_output where rule_id in (SELECT rule_id from rule_list where event_code = %s and bank_account_type = %t"),((param1, param2)))
